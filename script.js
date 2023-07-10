50c6a7a4da3628fa1471d4238ea3140cf674eefd
Joseph Tessitore <josephtessitore@protonmail.com>
2023-07-10 10:57:35 -0400

    Add a settings button
    Add a button to show the settings. They are now hidden by default

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const counter = document.querySelector(".counter");
 const sprite = document.querySelector(".sprite");
 const settings = document.querySelector(".settings");
 const settingsClose = document.querySelector(".settings-close");
+const settingsOpen = document.querySelector(".settings-open");
 
 let count = 0;
 let pokemon = 181;
@@ -15,4 +16,8 @@ settingsClose.addEventListener("click", function() {
     pokemon = document.querySelector("#pokemon-chooser").value;
     sprite.src = `sprites/sprites/pokemon/versions/generation-v/black-white/animated/shiny/${pokemon}.gif`
     settings.close();
+})
+
+settingsOpen.addEventListener("click", function() {
+    settings.showModal();
 })
