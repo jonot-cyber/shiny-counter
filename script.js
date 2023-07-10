b387c60234bc3f3a59a1ca21e9199d7b18f31436
Joseph Tessitore <josephtessitore@protonmail.com>
2023-07-10 10:49:41 -0400

    Add Pokemon Chooser to settings
    You can now type in the national pokedex # for a pokemon and change
    the preview to it

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const incrementButton = document.querySelector(".incr");
 const counter = document.querySelector(".counter");
+const sprite = document.querySelector(".sprite");
+const settings = document.querySelector(".settings");
+const settingsClose = document.querySelector(".settings-close");
 
 let count = 0;
+let pokemon = 181;
 
 incrementButton.addEventListener("click", function() {
     counter.innerText = ++count;
+})
+
+settingsClose.addEventListener("click", function() {
+    pokemon = document.querySelector("#pokemon-chooser").value;
+    sprite.src = `sprites/sprites/pokemon/versions/generation-v/black-white/animated/shiny/${pokemon}.gif`
+    settings.close();
 })
