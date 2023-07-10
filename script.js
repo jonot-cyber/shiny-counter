cc0c15193744401f5c32c18eb0d7f26bffb7a694
Joseph Tessitore <josephtessitore@protonmail.com>
2023-07-10 11:23:07 -0400

    Allow user to change what game is used.
    Gets the correct sprite for the selected game.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const incrementButton = document.querySelector(".incr");
 const counter = document.querySelector(".counter");
 const sprite = document.querySelector(".sprite");
+
 const settings = document.querySelector(".settings");
 const settingsClose = document.querySelector(".settings-close");
 const settingsOpen = document.querySelector(".settings-open");
+const pokemonChooser = document.querySelector("#pokemon-chooser");
+const gameChooser = document.querySelector("#game-chooser");
 
 let count = 0;
 let pokemon = 181;
@@ -13,8 +16,52 @@ incrementButton.addEventListener("click", function() {
 })
 
 settingsClose.addEventListener("click", function() {
-    pokemon = document.querySelector("#pokemon-chooser").value;
-    sprite.src = `sprites/sprites/pokemon/versions/generation-v/black-white/animated/shiny/${pokemon}.gif`
+    pokemon = pokemonChooser.value;
+    let game = gameChooser.value;
+    switch (game) {
+    case "au": // Gold
+	sprite.src = `sprites/sprites/pokemon/versions/generation-ii/gold/shiny/${pokemon}.png`;
+	break;
+    case "ag": // Silver
+	sprite.src = `sprites/sprites/pokemon/versions/generation-ii/silver/shiny/${pokemon}.png`;
+	break;
+    case "cr": // Crystal
+	sprite.src = `sprites/sprites/pokemon/versions/generation-ii/crystal/shiny/${pokemon}.png`;
+	break;
+    case "rs": // Ruby/Sapphire
+	sprite.src = `sprites/sprites/pokemon/versions/generation-iii/ruby-sapphire/shiny/${pokemon}.png`;
+	break;
+    case "frlg": // FireRed/LeafGreen
+	sprite.src = `sprites/sprites/pokemon/versions/generation-iii/firered-leafgreen/shiny/${pokemon}.png`;
+	break;
+    case "em": // Emerald
+	sprite.src = `sprites/sprites/pokemon/versions/generation-iii/emerald/shiny/${pokemon}.png`;
+	break;
+    case "dp": // Diamond/Pearl
+	sprite.src = `sprites/sprites/pokemon/versions/generation-iv/diamond-pearl/shiny/${pokemon}.png`;
+	break;
+    case "hgss": // HeartGold/SoulSilver
+	sprite.src = `sprites/sprites/pokemon/versions/generation-iv/heartgold-soulsilver/shiny/${pokemon}.png`;
+	break;
+    case "pt": // Platinum
+	sprite.src = `sprites/sprites/pokemon/versions/generation-iv/platinum/shiny/${pokemon}.png`;
+	break;
+    case "bw": // Black/White
+	sprite.src = `sprites/sprites/pokemon/versions/generation-v/black-white/animated/shiny/${pokemon}.gif`;
+	break;
+    case "xy": // X/Y
+	sprite.src = `sprites/sprites/pokemon/versions/generation-vi/x-y/shiny/${pokemon}.png`;
+	break;
+    case "oras": // Omega Ruby/Alpha Sapphire
+	sprite.src = `sprites/sprites/pokemon/versions/generation-vi/omegaruby-alphasapphire/shiny/${pokemon}.png`;
+	break;
+    case "sm": // Sun/Moon
+	sprite.src = `sprites/sprites/pokemon/versions/generation-vii/ultra-sun-ultra-moon/shiny/${pokemon}.png`;
+	break;
+    default:
+	sprite.src = `sprites/sprites/pokemon/versions/generation-v/black-white/animated/shiny/${pokemon}.gif`;
+	break;
+    }
     settings.close();
 })
 
